ba59cd4b30ccadcfeb78ca43554ab9692beed731
Julianne Kruse <juk99@pitt.edu>
2024-09-28 15:46:47 -0400

    made average difficulty function

--- rmp_test_code.py
+++ rmp_test_code.py
@@ -1,7 +1,7 @@
 import ratemyprofessor as rmp
 
 
-def get_prof_difficulty(prof_list: list) -> list:
+def get_prof_difficulty_average(prof_list: list) -> float:
 
     school = rmp.get_school_by_name("University of Pittsburgh")
     difficulty_list = []
@@ -9,10 +9,9 @@ def get_prof_difficulty(prof_list: list) -> list:
         prof = rmp.get_professor_by_school_and_name(school, name)
         if prof is not None:
             difficulty_list.append(prof.difficulty)
-    
-    return difficulty_list
+    return sum(difficulty_list)/len(difficulty_list)
 
 
 if __name__ == '__main__':
     test_list = ['William Garrison', 'Sherif Khattab']
-    print(get_prof_difficulty(test_list))
+    print(get_prof_difficulty_average(test_list))
