4bce10e15334f028981abca34ec5de824ec07379
HeyKile <bellokp2002@gmail.com>
2024-09-28 12:46:19 -0400

    created venv

---
+++ ai-schedule-venv/lib/python3.10/site-packages/PittAPI/dining.py
@@ -0,0 +1,97 @@
+"""
+The Pitt API, to access workable data of the University of Pittsburgh
+Copyright (C) 2015 Ritwik Gupta
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+import grequests
+from typing import Dict, List, Any
+
+
+def _get_all_locations():
+    """Creates a generator of responses to fetch data on all dining locations"""
+    request_objs = []
+    for i in range(3):
+        payload = {
+            "_kgoui_object": "kgoui_Rcontent_I2",
+            "feed": "dining_locations",
+            "start": i * 10
+        }
+        request_objs.append(grequests.get("https://m.pitt.edu/dining/index.json", params=payload))
+    resps = grequests.imap(request_objs)
+    return resps
+
+
+def get_locations():
+    """Gets information about all dining locations"""
+    return get_locations_by_status(None)
+
+
+def get_locations_by_status(status: str) -> List[Dict[str, Any]]:
+    """status can be nil, open, or closed
+    None    - returns all dining locations
+    'open'   - returns open dining locations
+    'closed' - returns closed dining locations"""
+
+    dining_locations = []
+    resps = [
+        r.json()["response"]['regions'][0]["contents"]
+        for r in _get_all_locations()
+    ]
+
+    for location in resps:
+        for content in location:
+            data = {}
+            fields = content["fields"]
+            if fields["type"] == "loadMore":
+                continue
+            if status in ['open', 'closed']:
+                if status != fields['status']:
+                    continue
+
+            if isinstance(fields["title"], dict):
+                data["name"] = fields["title"]["value"]
+            else:
+                data["name"] = fields["title"]
+            data["status"] = fields["status"]
+            try:
+                data["hours"] = fields["eventDate"]["formatted"]
+            except TypeError:
+                data["hours"] = "unavailable"
+
+            dining_locations.append(data)
+
+    return dining_locations
+
+
+# def get_location_by_name(location):
+#    try:
+#        return get_locations()[location]
+#    except:
+#        raise ValueError('The dining location is invalid')
+
+# def get_location_menu(location=None, date=None):
+# location can only be market, market's subordinates, and cathedral cafe
+# if location is none, return all menus, and date will be ignored
+# date has to be a day of the week, or if empty will return menus for all days of the week
+#
+# https://www.pc.pitt.edu/dining/menus/flyingStar.php
+# https://www.pc.pitt.edu/dining/menus/bellaTrattoria.php
+# https://www.pc.pitt.edu/dining/menus/basicKneads.php
+# https://www.pc.pitt.edu/dining/menus/basicKneads.php
+# https://www.pc.pitt.edu/dining/menus/magellans.php
+# https://www.pc.pitt.edu/dining/locations/cathedralCafe.php
+#    return []
---
+++ ai-schedule-venv/lib/python3.10/site-packages/PittAPI/people.py
@@ -0,0 +1,93 @@
+'''
+The Pitt API, to access workable data of the University of Pittsburgh
+Copyright (C) 2015 Ritwik Gupta
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
+import grequests
+from typing import List, Dict, Iterator, Any
+
+PEOPLE_URL = "https://m.pitt.edu/people/search.json"
+DETAIL_URL = "https://m.pitt.edu/people/detail.json"
+FIELD_MAP = {
+    "ldap:cn" : "name",
+    "ldap:buildingname": "address",
+    "kgoperson:phone" : "phone",
+    "ldap:ou" : "department",
+    "kgoperson:email" : "email",
+    "ldap:url" : "webpage",
+    "ldap:nickname" : "nickname",
+}
+
+def get_person(query: str, max_people: int=10) -> List[Dict[str,Any]]:
+    """ Returns a list of people """
+    id_list = [item for l_list in _get_person_id(query, max_people) for item in l_list]
+    results = [_get_person_details(id) for id in id_list]
+    people_info = [resp.json() for resp in grequests.map(results)]
+    persons = [_extract_person(person) for person in people_info]
+
+    return persons[:max_people]
+
+def _extract_person(item: Dict[str,Any]) -> Dict[str,Any]:
+    """ Returns person attributes """
+    kgoui_person = item['response']['regions'][0]['contents'][0]['fields'] \
+                       ['item']['value']['kgoDeflatedData']['attributes']
+
+    person = {FIELD_MAP[key]:kgoui_person[key].strip() for key in kgoui_person if key in FIELD_MAP}
+    return person
+
+
+
+def _get_person_details(id_number: str):
+    """ Returns unsent request """
+    payload = {
+        "search": "Search",
+        "id": id_number,
+        "_kgoui_object": "kgoui_Rcontent_I0_Rcontent_I1",
+    }
+    payload_str = "&".join("{}={}".format(k, v) for k, v in payload.items())
+    return grequests.get(DETAIL_URL, params=payload_str)
+
+
+def _get_person_id(query: str, max_people: int) -> List[Iterator[str]]:
+    """ Returns list of people IDs """
+    request_objs = []
+
+    for i in range(int(max_people / 10) + 1):
+        payload = {
+            "search": "Search",
+            "filter": query,
+            "_kgoui_region": "kgoui_Rcontent_I0_Rcontent_I0_Ritems",
+            "_object_include_html": 1,
+            "_object_js_config": 1,
+            "_kgoui_page_state": "439a1a9b6fb81b480ade61813e20e049",
+            "_region_index_offset": i * 10,
+            "feed": "directory",
+            "start": i * 10
+        }
+        request_objs.append(grequests.get(PEOPLE_URL, params=payload))
+
+    responses = grequests.imap(request_objs)
+
+    url_list = []
+    for response_obj in responses:
+        response = response_obj.json()['response']['contents']
+        local_url_list = (x['fields']['url']['formatted'] for x in response)
+        local_url_list = (dict(param.split("=") for param in x.split("&"))
+                          for x in local_url_list)
+        local_url_list = (x['id'] for x in local_url_list if 'id' in x)
+        url_list.append(local_url_list)
+    return url_list
