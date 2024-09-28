256920cfe412a6eb0367b17fdd68e5180256a12d
Ruchi Hegde <ruchi11.hegde@gmail.com>
2024-09-28 15:33:08 -0400

    formatted list of classes for home pg

--- ruchi-wow.py
+++ ruchi-wow.py
@@ -1,14 +1,18 @@
 import sys
 import os
+from typing import List
 sys.path.append(os.path.join(os.path.dirname(__file__), 'PittAPI'))
+print(sys.path)
 from pittapi import course
 
 cs_subject = course.get_subject_courses(subject='CS')
-courses_dict = cs_subject.courses
+courses_dict = cs_subject.courses # subject code, dictionary of courses
 
-print(courses_dict)
+def filtered_courses(courses:dict[str, course.Course]) -> List[str]:
+    parsed_courses = []
+    for key, course in courses.items():
+        parsed_courses.append(f"{course.subject_code} {course.course_number} {course.course_title}")
+    return parsed_courses
 
-cs_course = course.get_course_details(term='2244', subject='CS', course='1501')
-section_list = cs_course.sections
-
-print(section_list)
+parsed_courses = filtered_courses(courses_dict)
+print(parsed_courses)
